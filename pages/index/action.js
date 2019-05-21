466d0288ba9f6c26a4b2ae64786f911604bdeeeb
faipmardoni <faipmardoni@gmail.com>
2019-05-22 04:50:17 +0700

    add action getLatestRates

--- pages/index/action.js
+++ pages/index/action.js
@@ -1,7 +1,25 @@
-import { SET_SHOW_SELECT_AVAILABLE_CURRENCIES, SET_CURRENCIES_LIST } from './constant';
+import axios from 'axios';
 
-export const getLatestRates = () => {
+import {
+  SET_SHOW_SELECT_AVAILABLE_CURRENCIES, SET_CURRENCIES_LIST,
+  GET_LATEST_RATES_LOADING, GET_LATEST_RATES_ERROR, GET_LATEST_RATES,
+} from './constant';
 
+export const getLatestRates = () => (dispatch) => {
+  dispatch({ type: GET_LATEST_RATES_LOADING });
+  axios.get('https://api.exchangeratesapi.io/latest?base=USD')
+    .then((res) => {
+      dispatch({
+        type: GET_LATEST_RATES,
+        payload: res.data,
+      });
+    })
+    .catch((err) => {
+      dispatch({
+        type: GET_LATEST_RATES_ERROR,
+        payload: err.message,
+      });
+    });
 };
 
 export const setLatestRates = () => {
