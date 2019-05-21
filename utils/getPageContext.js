5d5232432fab42462e19f624aef0be0df2af1c3e
faipmardoni <faipmardoni@gmail.com>
2019-05-22 02:37:20 +0700

    change folder structure

---
+++ utils/getPageContext.js
@@ -0,0 +1,34 @@
+import { SheetsRegistry } from 'jss';
+import {
+  createMuiTheme,
+  createGenerateClassName,
+} from '@material-ui/core/styles';
+
+const theme = createMuiTheme({
+  typography: {
+    useNextVariants: true,
+  },
+});
+
+const createPageContext = () => ({
+  theme,
+  sheetsManager: new Map(),
+  sheetsRegistry: new SheetsRegistry(),
+  generateClassName: createGenerateClassName(),
+});
+
+let pageContext;
+
+const getPageContext = () => {
+  if (!process.browser) {
+    return createPageContext();
+  }
+
+  if (!pageContext) {
+    pageContext = createPageContext();
+  }
+
+  return pageContext;
+};
+
+export default getPageContext;
